9595901858eafb44657851434faf0a8be7c56d48
Daniel Kincses <danikincs@gmail.com>
2022-08-30 17:48:16 +0200

    implement drivers GET endpoint

--- api/src/controllers/drivers.controller.ts
+++ api/src/controllers/drivers.controller.ts
@@ -1,10 +1,21 @@
-export function getDriversController() {
+import { Response, Request } from "express"
+import { drivers } from "../app"
 
-    //implement
-    return ""
+/**
+ * Get all the drivers
+ * 
+ * TODO: This controller is probably unnecessary for the current setup, but the architecture would be the same if we store drivers in DB.
+ * 
+ * @param req Express request
+ * @param res Express response
+ */
+export function getDriversController(req:Request, res:Response) {
+
+    let driversRes = drivers;
+    res.status(200).send(driversRes)
 }
 
-export function overtakeDriverController() {
+export function overtakeDriverController(req:Request, res:Response) {
 
     //implement
     return ""
