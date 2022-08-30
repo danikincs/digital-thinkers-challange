cb08d92508e6020f83bb7b3f5df36500766afebd
Daniel Kincses <danikincs@gmail.com>
2022-08-30 18:19:22 +0200

    fix public folder placement, and serve to public

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,14 +1,18 @@
 import express from "express";
-import loadJson from "./_helpers/load_json";
 
 import * as dotenv from "dotenv";
 dotenv.config();
 
 //Import routers
 import router from "./router/drivers.route";
+import path from "path";
 
 const app = express();
 
+//provide images in public folder
+var dir = path.join(__dirname, 'public');
+
+app.use(express.static(dir));
 //routers
 app.use("/api", router);
 
